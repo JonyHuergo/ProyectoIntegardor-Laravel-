4d7096fca5d98a32409d28743d2a724a04be9885
JonyHuergo <jh2000@hotmail.com.ar>
2018-07-19 13:03:39 -0300

    Crecion del archivo blade para mostrar cada cancha

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -9,7 +9,7 @@
                 <li class="{{ Request::is( 'canchas') ? 'active' : '' }}"><a href="canchas">Canchas</a></li>
                 <li class="{{ Request::is( 'preguntas') ? 'active' : '' }}"><a href="preguntas">Preguntas</a></li>
                 <li class="{{ Request::is( 'registro') ? 'active' : '' }}"><a href="registro">Registrarse</a></li>
-                <li class="{{ Request::is( 'ingresar') ? 'active' : '' }}"><a href="login">Ingresar</a></li>
+                <li class="{{ Request::is( 'login') ? 'active' : '' }}"><a href="login">Ingresar</a></li>
               </ul>
           </div>
         </nav>
---
+++ resources/views/showCancha.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('title', 'Canchas')
+@section('style')
+<link rel="stylesheet" href="{{ URL::asset('css/registroStyle.css') }}">
+@endsection
+@section('content')
+
+@endsection
