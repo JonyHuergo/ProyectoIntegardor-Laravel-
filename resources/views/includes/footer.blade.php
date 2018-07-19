f4b44f8f99073d576e9bb262e6075bd47405de4c
JonyHuergo <jh2000@hotmail.com.ar>
2018-07-19 20:49:52 -0300

    Bug fix en paginas individuales de canchas

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,9 @@
 <!-- Footer -->
 <div>
-          <ul>
-            <li><a href="https://www.instagram.com/?hl=es-la" target="blank"><div class="footer-icons"><i class="fa fa-instagram fa-2x"></i></div></a></li>
-            <li><a href="https://twitter.com/?lang=es/" target="blank"><div class="footer-icons"><i class="fa fa-twitter fa-2x"></i></div></a></li>
-            <li><a href="https://es-la.facebook.com/" target="blank"><div class="footer-icons"><i class="fa fa-facebook fa-2x"></i></div></a></li>
-          </ul>
-        </div>
+  <ul>
+    <li><a href="https://www.instagram.com/?hl=es-la" target="blank"><div class="footer-icons"><i class="fa fa-instagram fa-2x"></i></div></a></li>
+    <li><a href="https://twitter.com/?lang=es/" target="blank"><div class="footer-icons"><i class="fa fa-twitter fa-2x"></i></div></a></li>
+    <li><a href="https://es-la.facebook.com/" target="blank"><div class="footer-icons"><i class="fa fa-facebook fa-2x"></i></div></a></li>
+  </ul>
+</div>
 <!-- Footer END -->
