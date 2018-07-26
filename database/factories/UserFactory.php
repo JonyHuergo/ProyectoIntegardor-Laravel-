7f1ee49cf646ea19c7fc2523791ec005636316a4
JonyHuergo <jh2000@hotmail.com.ar>
2018-07-25 23:23:44 -0300

    Agregado de factories y seeders para la base de datos

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,23 +1,17 @@
 <?php
 
-use Faker\Generator as Faker;
+use Faker\Generator;
+use App\User;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Generator $faker) {
+    $faker = Faker\Factory::create('es_AR');
     return [
-        'name' => $faker->name,
+        'first_name' => $faker->optional()->firstName,
+        'last_name' => $faker->optional()->lastName,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => bcrypt($faker->password),
         'remember_token' => str_random(10),
+        'username' => $faker->unique()->userName,
+        'avatar' => $faker->optional()->imageUrl($width = 200, $height = 200),
     ];
 });
