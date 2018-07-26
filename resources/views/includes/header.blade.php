1ef76d0502433876f5f97b92ef00a1ae473cdf2d
Natalia Calabrese <natalia@metamorfosys.com.ar>
2018-07-25 21:33:28 -0300

    ajustes CSS slider

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,7 @@
 <!-- Header -->
+<div class="container">
   <div class="logo col-md-4" id="logo">
     <a href="/"><img src="{{ URL::asset('imagenes/logo2.png') }}"></a>
   </div>
+</div>
 <!-- Header END -->
